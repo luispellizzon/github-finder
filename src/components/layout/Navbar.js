086d94cfcc786b042d07d7c1b6e3f4d9c7b2ac88
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-08-04 21:49:28 +0100

    Github Finder React App

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { FaGithub } from 'react-icons/fa'
+
+const Navbar = ({title}) => {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content text-white">
+      {/* Links on the Left*/}
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <Link to='/' className='text-lg font-bold align-middle'>
+            <FaGithub className='inline pr-2 text-3xl' />
+            {title}
+          </Link>
+        </div>
+         {/* Links on the Right*/}
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+              Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+              About
+            </Link>
+          </div>
+        </div>
+
+      </div>
+    </nav>
+  )
+}
+
+Navbar.defaultProps ={
+  title: 'Github Finder'
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+}
+
+export default Navbar
