7df794b97f2a190c17f6ce6edfb03ee2057fda5d
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-08-05 21:40:23 +0100

    404-Not Found Component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaHome } from 'react-icons/fa';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
