207ce8fc98cdb6738a97670340dcd3848e5414a2
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-08-05 23:06:50 +0100

    UserResults component updated to use the Github Context from global state

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,33 +1,24 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useContext} from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 
 function UserResults() {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
 
+    const { fetchUsers, users, loading } = useContext(GithubContext)
     useEffect(()=>{
         fetchUsers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
 
-const fetchUsers = async () =>{
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-        headers:{
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-        }
-    });
-    
-    const data = await response.json();
-    setUsers(data)
-    setLoading(false);
-}
     if(!loading) {
         
         return (
          
           <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2"> 
               {users.map((user, key) => (
-                  <div key={key}> {user.login}</div>
+                  <UserItem key={user.id} user={user}/>
               ))}
           
           </div>
