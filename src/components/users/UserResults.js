dcaa97db7242feedfd3d400a7b03a80afd7a4643
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-22 21:05:18 +0100

    Cleaning fetchUsers() fn from userResults.js because we do not need it anymore since we are fetching from the global context

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,32 +1,22 @@
-
-import React, { useEffect, useContext} from 'react';
-import Spinner from '../layout/Spinner';
-import UserItem from './UserItem';
-import GithubContext from '../../context/github/GithubContext';
+import React, { useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
+  const { users, loading } = useContext(GithubContext);
 
-    const { fetchUsers, users, loading } = useContext(GithubContext)
-    useEffect(()=>{
-        fetchUsers()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-
-    if(!loading) {
-        
-        return (
-         
-          <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2"> 
-              {users.map((user, key) => (
-                  <UserItem key={user.id} user={user}/>
-              ))}
-          
-          </div>
-          
-        )
-    } else {
-        return <Spinner />
-    }
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user, key) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
-export default UserResults
+export default UserResults;
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,6 +6,12 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
+
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return state;
   }
