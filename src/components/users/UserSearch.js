afcfa60cedbaf76574b6be6a48f0af421b956450
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-23 22:31:58 +0100

    Search users functionality working

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -3,7 +3,7 @@ import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
-  const { users } = useContext(GithubContext);
+  const { users, searchUsers } = useContext(GithubContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -12,7 +12,9 @@ function UserSearch() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    text === "" ? alert("Please enter a Github user") : setText("");
+    text === ""
+      ? alert("Please enter a Github user")
+      : searchUsers(text) && setText("");
   };
 
   return (
