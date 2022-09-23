40aa1003c40833e8bfca034847aedbeb24f497c0
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-23 22:24:36 +0100

    Clear button to show only when there is at least one user on state

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
+  const { users } = useContext(GithubContext);
+
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
-  console.log(text);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    text === "" ? alert("Please enter a Github user") : setText("");
+  };
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
@@ -28,9 +35,11 @@ function UserSearch() {
           </div>
         </form>
       </div>
-      <div>
-        <button className="btn btn-ghost btn-lg">Clear</button>
-      </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        </div>
+      )}
     </div>
   );
 }
