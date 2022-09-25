67e7ecac249641802c84b23d4cd3f7cc9f5ce71c
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-25 18:03:43 +0100

    Embed UserSearch with the AlertContext

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -1,9 +1,12 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+
+  const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -13,7 +16,7 @@ function UserSearch() {
     e.preventDefault();
 
     text === ""
-      ? alert("Please enter a Github user")
+      ? setAlert("Please enter a Github user", "error")
       : searchUsers(text) && setText("");
   };
 
