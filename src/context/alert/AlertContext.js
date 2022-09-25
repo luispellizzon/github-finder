cf741deeca9fd2d153f7d0ed64f6919bd35c92fa
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-25 17:47:16 +0100

    Creating AlertContext & AlertReducer for alerts

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,8 @@
+import { createContext, useReducer } from "react";
+import alertReducer from "./AlertReducer";
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+  const initalState = null;
+};
