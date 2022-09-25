68608ad0070306be2039c1768703fe8e7eec188e
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-25 17:50:24 +0100

    AlertContext hooked up on App.js

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -5,4 +5,14 @@ const AlertContext = createContext();
 
 export const AlertProvider = ({ children }) => {
   const initalState = null;
+
+  const [state, dispatch] = useReducer(alertReducer, initalState);
+
+  return (
+    <AlertContext.Provider value={{ alert: state }}>
+      {children}
+    </AlertContext.Provider>
+  );
 };
+
+export default AlertContext;
