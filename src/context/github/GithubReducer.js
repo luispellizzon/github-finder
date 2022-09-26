646e881def52913f48419a500c6a61839d18d80c
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-26 17:51:43 +0100

    Adding User Page when user click to view the github profile. Router created for User Component & user State added on Global Context so it will fetch the specific github user when view profile link is clicked' '

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -17,6 +17,13 @@ const githubReducer = (state, action) => {
         ...state,
         users: [],
       };
+
+    case "GET_USER":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
     default:
       return state;
   }
