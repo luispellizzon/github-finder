efa657be1815032c75b974941a99a6e82855ca21
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-27 17:11:48 +0100

    Changed Top to Updated repositiores title on RepoList component

--- src/components/repos/RepoList.js
+++ src/components/repos/RepoList.js
@@ -4,7 +4,9 @@ function RepoList({ repos }) {
   return (
     <div className="rounded-lg shadow-lg card bg-base-100">
       <div className="card-body">
-        <h2 className="text-3xl my-4 font-bold card-title">Top Repositories</h2>
+        <h2 className="text-3xl my-4 font-bold card-title">
+          Updated Repositories
+        </h2>
 
         {repos.map((repo) => (
           <h3>{repo.name}</h3>
