035e4514cbad3026af30dab448fa5c00a6361c9f
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-27 18:41:07 +0100

    Final modification: Deployed to Vercel and added enviroment variables, so the token I was using to fetch the data from the Github API is now secured. Deleted from the Folder.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { FaGithub } from 'react-icons/fa'
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { FaGithub } from "react-icons/fa";
 
-const Navbar = ({title}) => {
+const Navbar = ({ title }) => {
   return (
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       {/* Links on the Left*/}
       <div className="container mx-auto text-white">
         <div className="flex-none px-2 mx-2">
-          <Link to='/' className='text-lg font-bold align-middle'>
-            <FaGithub className='inline pr-2 text-3xl ' />
+          <Link to="/" className="text-lg font-bold align-middle">
+            <FaGithub className="inline pr-2 text-3xl " />
             {title}
           </Link>
         </div>
-         {/* Links on the Right*/}
+        {/* Links on the Right*/}
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
             </Link>
-            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
               About
             </Link>
           </div>
         </div>
-
       </div>
     </nav>
-  )
-}
+  );
+};
 
-Navbar.defaultProps ={
-  title: 'Github Finder'
-}
+Navbar.defaultProps = {
+  title: "QuickHub Finder",
+};
 
 Navbar.propTypes = {
   title: PropTypes.string,
-}
+};
 
-export default Navbar
+export default Navbar;
